afdeecb269d634149a1579980b7cd72e6ff0ff2b
tfaassen <114919732+tfaassen@users.noreply.github.com>
2025-03-25 10:17:38 +0100

    laatste versie waar ik mee bezig ben

--- gip_project/geozoekkr/backend/routes/gamerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { submitScore, getLeaderboard } = require("./controllers/gameController");
-const router = express.Router();
-
-router.post("/submit-score", submitScore);
-router.get("/leaderboard", getLeaderboard);
-
-module.exports = router;
